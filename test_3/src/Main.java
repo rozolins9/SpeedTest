a468644e486d9af77e2b44685438bee4b14682c7
rozolins9 <103949057+rozolins9@users.noreply.github.com>
2022-05-31 20:13:29 +0300

    Add files via upload

---
+++ test_3/src/Main.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.URL;
+
+public class Main {
+    public static void main(String[] args) {
+        Server server = new Server(5050);
+
+
+//        try {
+//
+//            InetAddress add = InetAddress.getByName("192.168.0.100");
+//            String a = add.getHostName();
+//            System.out.println(a);
+//            boolean reachable = add.isReachable(1000);
+//            System.out.println(reachable);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+////
+    }
+}
